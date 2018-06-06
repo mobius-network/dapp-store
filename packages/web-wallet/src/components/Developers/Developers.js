5d0742dcaa712b75b32d96873e4d6c364b312b0f
Valery Bugakov <skymk1@gmail.com>
2018-06-06 11:02:11 +0400

    Hide MyDApps and Submit DApp routes for now

--- packages/web-wallet/src/components/Developers/Developers.js
+++ packages/web-wallet/src/components/Developers/Developers.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
-// import { string } from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from 'components/shared/PrivateRoute';
 
-import MyDapps from './MyDapps';
-import SubmitDapp from './SubmitDapp';
+// import MyDapps from './MyDapps';
+// import SubmitDapp from './SubmitDapp';
 import GettingStarted from './GettingStarted';
 import { Container, Title } from './styles';
 
 class Developers extends Component {
-  static propTypes = {
-    // name: string.isRequired,
-  };
-
   render() {
     return (
       <Container>
-        <Title>Developers</Title>
+        <Title>Developers Section</Title>
         <Switch>
           <PrivateRoute
             path="/developers"
             redirectTo="start"
-            component={MyDapps}
-            exact
-          />
-          <PrivateRoute
-            path="/developers/submit"
-            redirectTo="start"
-            component={SubmitDapp}
+            component={GettingStarted}
             exact
           />
           <Route path="/developers/start" component={GettingStarted} />
