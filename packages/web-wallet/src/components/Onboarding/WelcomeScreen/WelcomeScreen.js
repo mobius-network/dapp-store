55a850622655cc4b57c6eedc5eacc1a6111906f3
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-08 02:17:00 +0200

    Add OnboardingLayout w/ welcome screen styling

--- packages/web-wallet/src/components/Onboarding/WelcomeScreen/WelcomeScreen.js
+++ packages/web-wallet/src/components/Onboarding/WelcomeScreen/WelcomeScreen.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
-// import { string } from 'prop-types';
-import { Link } from 'react-router-dom';
 
-import { Container, Title } from './styles';
+import Button from 'components/shared/Button';
+import {
+  Container,
+  Header,
+  HeaderLogo,
+  HeaderTitle,
+  HeaderCaption,
+  Content,
+  ContentSteps,
+  ContentStep,
+  ContentStepIcon,
+  ContentStepText,
+} from './styles';
 
 class WelcomeScreen extends Component {
-  static propTypes = {
-    // name: string.isRequired,
-  };
-
   render() {
     return (
-      <Container>
-        <Title>WelcomeScreen</Title>
-        <Link to="/onboarding/xlm">Continue</Link>
+      <Container theme="wide">
+        <Header>
+          <HeaderLogo theme="dark" />
+          <HeaderTitle>Welcome to Mobius!</HeaderTitle>
+          <HeaderCaption>
+            Let’s finish setting up your Mobius wallet
+          </HeaderCaption>
+        </Header>
+
+        <Content>
+          <ContentSteps>
+            <ContentStep>
+              <ContentStepIcon>1</ContentStepIcon>
+              <ContentStepText>Transfer XLM to your new wallet</ContentStepText>
+            </ContentStep>
+            <ContentStep>
+              <ContentStepIcon>2</ContentStepIcon>
+              <ContentStepText>Add MOBI to your wallet</ContentStepText>
+            </ContentStep>
+            <ContentStep>
+              <ContentStepIcon>3</ContentStepIcon>
+              <ContentStepText>Start making purchases in DApps</ContentStepText>
+            </ContentStep>
+          </ContentSteps>
+          <Button to="/onboarding/xlm" fullWidth>
+            Continue
+          </Button>
+        </Content>
       </Container>
     );
   }
