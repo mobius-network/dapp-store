0f5c104df0c48932a951dd8873a3f43868243626
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-08 20:35:06 +0200

    Add theming to shared Link component

--- packages/web-wallet/src/components/shared/Link/Link.js
+++ packages/web-wallet/src/components/shared/Link/Link.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
 
+import { colors } from 'components/shared/Styleguide';
 import { StyledLink, StyledHyperlink } from './styles';
 
-const Link = ({ to, href, ...rest }) =>
-  to ? (
-    <StyledLink to={to} {...rest} />
-  ) : (
-    <StyledHyperlink
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      {...rest}
-    />
-  );
-
-Link.propTypes = {
-  href: PropTypes.string,
-  to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+const themes = {
+  default: {
+    color: colors.textPrimary,
+  },
+  light: {
+    color: colors.textWhite,
+  },
 };
+
+class Link extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    href: PropTypes.string,
+    theme: PropTypes.oneOf(['default', 'light']),
+    to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  };
+
+  static defaultProps = {
+    theme: 'default',
+  };
+
+  render() {
+    const {
+      className, href, theme, to, ...rest
+    } = this.props;
+
+    return (
+      <ThemeProvider theme={themes[theme]}>
+        {to ? (
+          <StyledLink className={className} to={to} {...rest} />
+        ) : (
+          <StyledHyperlink
+            className={className}
+            href={href}
+            rel="noopener noreferrer"
+            target="_blank"
+            {...rest}
+          />
+        )}
+      </ThemeProvider>
+    );
+  }
+}
 
 export default Link;
