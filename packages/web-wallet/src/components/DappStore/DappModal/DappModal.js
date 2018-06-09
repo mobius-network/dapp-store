60307dd39c4d85c58abe9cded0950a688ee97065
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-09 03:29:37 +0200

    Add DApp Store modal styling

--- packages/web-wallet/src/components/DappStore/DappModal/DappModal.js
+++ packages/web-wallet/src/components/DappStore/DappModal/DappModal.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { object, bool, func } from 'prop-types';
 import Modal from 'react-modal';
 
+import { colors, radius } from 'components/shared/Styleguide';
+import Grid from 'components/shared/Grid';
+import Button from 'components/shared/Button';
 import DepositModal from 'components/shared/DepositModal';
-import { Container, Title } from './styles';
+import {
+  AppBalance,
+  AppBalanceAmount,
+  AppDetails,
+  AppName,
+  AppPic,
+  ButtonRow,
+  Content,
+  Desc,
+} from './styles';
 
 Modal.setAppElement('#root');
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
 
 class DappModal extends Component {
   static propTypes = {
     app: object.isRequired,
-    onClose: func.isRequired,
     isOpen: bool.isRequired,
+    onClose: func.isRequired,
     style: object,
+  };
+
+  static defaultProps = {
+    mobiBalance: 0,
+    style: {
+      overlay: {
+        backgroundColor: colors.overlay,
+      },
+      content: {
+        borderRadius: radius.default,
+        bottom: 'auto',
+        left: '50%',
+        marginRight: '-50%',
+        padding: '30px',
+        right: 'auto',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      },
+    },
   };
 
   state = {
@@ -48,36 +68,61 @@ class DappModal extends Component {
     const { depositModalOpened } = this.state;
 
     const {
-      isOpen,
-      style = customStyles,
-      onClose,
-      app,
-      mobiBalance,
+      isOpen, style, onClose, app, mobiBalance,
     } = this.props;
 
     return (
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onClose}
-        contentLabel="Dapp modal"
-        style={style}
-      >
-        <Title>{app.name}</Title>
-        <button onClick={onClose}>close</button>
-        <Container>
-          <button onClick={this.openApp}>Go to App</button>
-          <button onClick={this.openDepositModal}>
-            Open DepositModal Modal
-          </button>
-          <p>Mobi balance: {mobiBalance}</p>
-        </Container>
-
+      <Fragment>
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={onClose}
+          contentLabel="Dapp modal"
+          style={style}
+        >
+          <Content>
+            <Grid>
+              <Grid.Row>
+                <Grid.Col px={0}>
+                  <AppDetails>
+                    <AppPic url={app.image_url} />
+                    <AppName>{app.name}</AppName>
+                  </AppDetails>
+                </Grid.Col>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Col px={0} width={7 / 10}>
+                  <Desc>{app.description}</Desc>
+                </Grid.Col>
+                <Grid.Col px={0} width={3 / 10}>
+                  <ButtonRow>
+                    <Button onClick={this.openApp} fullWidth>
+                      Go to App
+                    </Button>
+                  </ButtonRow>
+                  <ButtonRow>
+                    <Button
+                      onClick={this.openDepositModal}
+                      fullWidth
+                      theme="secondary"
+                    >
+                      Deposit Funds
+                    </Button>
+                  </ButtonRow>
+                  <AppBalance>
+                    DApp balance:{' '}
+                    <AppBalanceAmount>{mobiBalance} MOBI</AppBalanceAmount>
+                  </AppBalance>
+                </Grid.Col>
+              </Grid.Row>
+            </Grid>
+          </Content>
+        </Modal>
         <DepositModal
           app={app}
           isOpen={depositModalOpened}
           onClose={this.closeDepositModal}
         />
-      </Modal>
+      </Fragment>
     );
   }
 }
--- packages/web-wallet/src/components/DappStore/DappModal/styles.js
+++ packages/web-wallet/src/components/DappStore/DappModal/styles.js
@@ -1,5 +1,59 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import Pic from 'components/shared/Pic';
+import {
+  colors,
+  breakpoints,
+  fontSizes,
+  fonts,
+  lineHeights,
+} from 'components/shared/Styleguide';
 
-export const Title = styled.p``;
+export const Content = styled.div`
+  @media screen and (min-width: ${breakpoints.md}) {
+    width: ${breakpoints.md};
+  }
+`;
+
+export const AppDetails = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 0 40px;
+`;
+
+export const AppPic = styled(Pic)`
+  margin-right: 20px;
+  width: 90px;
+  height: 90px;
+`;
+
+export const AppName = styled.p`
+  font-size: 20px;
+  margin: 0;
+  flex: 1;
+  font-weight: 700;
+  font: 700 ${fontSizes.subHeading} ${fonts.default};
+`;
+
+export const Desc = styled.p`
+  border-top: 1px solid ${colors.border};
+  font: ${fontSizes.paneCaption} / ${lineHeights.default} ${fonts.default};
+  margin: 0 20px 0 0;
+  padding: 30px 0 0;
+`;
+
+export const ButtonRow = styled.div`
+  margin: 0 0 10px;
+`;
+
+export const AppBalance = styled.p`
+  font: 13px ${fonts.default};
+  color: ${colors.textSecondary};
+  margin: 0;
+  text-align: center;
+`;
+
+export const AppBalanceAmount = styled.span`
+  color: ${colors.textPrimary};
+  font-weight: 700;
+`;
