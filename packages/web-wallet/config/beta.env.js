87b063b6e6cb43f91db461cefe636623d6575716
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-15 18:45:26 +0200

    Add public network env config alongside w/ beta build config

--- packages/web-wallet/config/beta.env.js
+++ packages/web-wallet/config/beta.env.js
@@ -6,9 +6,9 @@ module.exports = {
   API_URL: '"https://beta.mobius.network/api/v1"',
   COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
   HONEYBADGER_API_TOKEN: '"d52d00a4"',
-  MOBI_ASSET_ISSUER: '"GA6HCMBLTZS5VYYBCATRBRZ3BZJMAFUDKYYF6AH6MVCMGWMRDNSWJPIH"',
+  MOBI_ASSET_ISSUER: '"GDRWBLJURXUKM4RWDZDTPJNX6XBYFO3PSE4H4GPUL6H6RCUQVKTSD4AT"',
   MOBI_ASSET: '"MOBI"',
-  NODE_ENV: '"production"',
-  STELLAR_HORIZON_URL: '"https://horizon.stellar.org"',
-  STELLAR_TESTNET: '"false"',
+  NODE_ENV: '"beta"',
+  STELLAR_HORIZON_URL: '"https://horizon-testnet.stellar.org"',
+  STELLAR_TESTNET: '"true"',
 };
