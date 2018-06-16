349ccd824787a00fa6536a16df10e5cab302b7b6
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-16 02:07:30 +0200

    Fix Stellar Network initi in production env

--- packages/core/src/services/stellar/server.js
+++ packages/core/src/services/stellar/server.js
@@ -2,6 +2,8 @@ import { Server, Network } from 'stellar-sdk';
 
 if (process.env.STELLAR_TESTNET === 'true') {
   Network.useTestNetwork();
+} else {
+  Network.usePublicNetwork();
 }
 
 export const stellarServer = new Server(process.env.STELLAR_HORIZON_URL);
