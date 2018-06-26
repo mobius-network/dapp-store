7b43ba946b1c844f89a68f64a251e01dd36e38ce
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-26 16:14:33 +0200

    Add ability to customize modal w/ header & close button

--- packages/web-wallet/src/components/shared/Modal/styles.js
+++ packages/web-wallet/src/components/shared/Modal/styles.js
@@ -6,6 +6,8 @@ import {
   colors,
   radius,
   shadows,
+  fonts,
+  fontSizes,
 } from 'components/shared/Styleguide';
 
 export const StyledReactModal = styled(ReactModal)`
@@ -31,5 +33,44 @@ export const Content = styled.div`
   background: ${colors.bgWhite};
   border-radius: ${radius.default};
   box-shadow: ${shadows.modal};
+  position: relative;
+`;
+
+export const Header = styled.div`
+  border-bottom: 1px solid ${colors.border};
+  padding: 20px 30px;
+`;
+
+export const HeaderTitle = styled.p`
+  color: ${colors.textDefault};
+  font-family: ${fonts.default};
+  font-size: ${fontSizes.heading};
+  font-weight: 300;
+  margin: 0;
+`;
+
+export const CloseButton = styled.button`
+  background: none;
+  border: none;
+  box-shadow: none;
+  color: #c8c8c8;
+  cursor: pointer;
+  font-size: 20px;
+  line-height: 30px;
+  margin: 0;
+  opacity: 1;
+  outline: 0;
+  padding: 0;
+  position: absolute;
+  right: 30px;
+  top: 20px;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const Body = styled.div`
   padding: 30px;
 `;
