bc57845097b06fb0bb1c2b1b4daf9370fda3e8fc
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-27 08:55:00 +0200

    Cleanup DappModal component markup

--- packages/web-wallet/src/components/DappStore/DappModal/DappModal.js
+++ packages/web-wallet/src/components/DappStore/DappModal/DappModal.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { object, bool, func } from 'prop-types';
 import { faAngleRight } from '@fortawesome/fontawesome-free-solid';
 
@@ -32,63 +32,61 @@ class DappModal extends Component {
     } = this.props;
 
     return (
-      <Fragment>
-        <Modal
-          closeButton
-          contentLabel="DApp Modal"
-          isOpen={isOpen}
-          onRequestClose={onClose}
-        >
-          <Grid>
-            <Grid.Row>
-              <Grid.Col px={0} width={1}>
-                <AppHeader>
-                  <AppPic url={app.image_url} />
-                  <AppDetails>
-                    <AppName>{app.name}</AppName>
-                    {app.tagline && <AppTagline>{app.tagline}</AppTagline>}
-                  </AppDetails>
-                </AppHeader>
-              </Grid.Col>
-            </Grid.Row>
-            <Grid.Row flexWrap="wrap">
-              <Grid.Col px={0} mb={[30, 30, 0]} width={[1, 1, 7 / 10]}>
-                <Desc>{app.description}</Desc>
-              </Grid.Col>
-              <Grid.Col px={0} width={[1, 1, 3 / 10]}>
-                <BalanceButtons app={app} mobiBalance={mobiBalance} />
+      <Modal
+        closeButton
+        contentLabel="DApp Modal"
+        isOpen={isOpen}
+        onRequestClose={onClose}
+      >
+        <Grid>
+          <Grid.Row>
+            <Grid.Col px={0} width={1}>
+              <AppHeader>
+                <AppPic url={app.image_url} />
+                <AppDetails>
+                  <AppName>{app.name}</AppName>
+                  {app.tagline && <AppTagline>{app.tagline}</AppTagline>}
+                </AppDetails>
+              </AppHeader>
+            </Grid.Col>
+          </Grid.Row>
+          <Grid.Row flexWrap="wrap">
+            <Grid.Col px={0} mb={[30, 30, 0]} width={[1, 1, 7 / 10]}>
+              <Desc>{app.description}</Desc>
+            </Grid.Col>
+            <Grid.Col px={0} width={[1, 1, 3 / 10]}>
+              <BalanceButtons app={app} mobiBalance={mobiBalance} />
 
-                {(app.website_url || app.support_url) && (
-                  <AppLinks>
-                    {app.website_url && (
-                      <AppLink
-                        href={app.website_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <AppLinkTitle>Website</AppLinkTitle>
-                        <AppLinkUrl>{app.website_url}</AppLinkUrl>
-                        <AppLinkArrow icon={faAngleRight} />
-                      </AppLink>
-                    )}
+              {(app.website_url || app.support_url) && (
+                <AppLinks>
+                  {app.website_url && (
+                    <AppLink
+                      href={app.website_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <AppLinkTitle>Website</AppLinkTitle>
+                      <AppLinkUrl>{app.website_url}</AppLinkUrl>
+                      <AppLinkArrow icon={faAngleRight} />
+                    </AppLink>
+                  )}
 
-                    {app.support_url && (
-                      <AppLink
-                        href={app.support_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <AppLinkTitle>Support</AppLinkTitle>
-                        <AppLinkArrow icon={faAngleRight} />
-                      </AppLink>
-                    )}
-                  </AppLinks>
-                )}
-              </Grid.Col>
-            </Grid.Row>
-          </Grid>
-        </Modal>
-      </Fragment>
+                  {app.support_url && (
+                    <AppLink
+                      href={app.support_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <AppLinkTitle>Support</AppLinkTitle>
+                      <AppLinkArrow icon={faAngleRight} />
+                    </AppLink>
+                  )}
+                </AppLinks>
+              )}
+            </Grid.Col>
+          </Grid.Row>
+        </Grid>
+      </Modal>
     );
   }
 }
