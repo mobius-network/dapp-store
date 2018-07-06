29ca7aade96089a6c52161ebb397829af69bd304
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-06 19:31:39 +0200

    [MOB-295] Replace unused "Getting Started" page w/ docs.mobius.network links for non-authorized users

--- packages/web-wallet/src/components/Developers/Developers.js
+++ packages/web-wallet/src/components/Developers/Developers.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import PrivateRoute from 'components/shared/PrivateRoute';
 
 import SubmitDapp from './SubmitDapp';
-import GettingStarted from './GettingStarted';
 
 class Developers extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/developers" component={GettingStarted} exact />
         <PrivateRoute path="/developers/submit" component={SubmitDapp} exact />
       </Switch>
     );
