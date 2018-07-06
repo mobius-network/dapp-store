c9fb0f4dfc7722207d5746da48e692aa0716f31b
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-06 16:42:40 +0200

    [MOB-295] Add SubmitDapp form w/ validations

--- packages/web-wallet/src/components/Developers/Developers.js
+++ packages/web-wallet/src/components/Developers/Developers.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import PrivateRoute from 'components/shared/PrivateRoute';
 
-// import MyDapps from './MyDapps';
-// import SubmitDapp from './SubmitDapp';
+import PrivateRoute from 'components/shared/PrivateRoute';
+
+import SubmitDapp from './SubmitDapp';
 import GettingStarted from './GettingStarted';
 
 class Developers extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/developers" component={GettingStarted} />
+        <Route path="/developers" component={GettingStarted} exact />
+        <PrivateRoute path="/developers/submit" component={SubmitDapp} exact />
       </Switch>
     );
   }
--- packages/web-wallet/src/components/Developers/SubmitDapp/SubmitDapp.js
+++ packages/web-wallet/src/components/Developers/SubmitDapp/SubmitDapp.js
@@ -1,55 +1,112 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Pane from 'components/shared/Pane';
+import Grid from 'components/shared/Grid';
+import FormRow from 'components/shared/FormRow';
 import TextInput from 'components/shared/TextInput';
-import FileInput from 'components/shared/FileInput';
 import Textarea from 'components/shared/Textarea';
-
-import {
-  Container,
-  Title,
-  Form,
-  TextField,
-  UploadField,
-  SubmitButton,
-} from './styles';
-
-// TODO: move to utils
-const required = v => !!v;
+import Button from 'components/shared/Button';
 
 class SubmitDapp extends Component {
+  static propTypes = {
+    t: PropTypes.func.isRequired,
+  };
+
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, t } = this.props;
 
     return (
-      <Container>
-        <Title>Submit DApp</Title>
-
-        <Form onSubmit={handleSubmit}>
-          <TextField name="name" component={TextInput} validate={required} />
-          <TextField name="url" component={TextInput} validate={required} />
-
-          <TextField name="summary" component={TextInput} validate={required} />
-          <TextField
-            name="description"
-            component={Textarea}
-            validate={required}
-          />
-
-          <UploadField
-            name="icon"
-            component={FileInput}
-            validate={required}
-            onChange={this.onKeyUpload}
-          />
-          <UploadField
-            name="screenshots"
-            component={FileInput}
-            validate={required}
-            onChange={this.onKeyUpload}
-          />
-
-          <SubmitButton onClick={handleSubmit}>Submit Draft</SubmitButton>
-        </Form>
-      </Container>
+      <Pane theme="narrow">
+        <Pane.Header title={t('submitDapp.title')} />
+        <form onSubmit={handleSubmit}>
+          <Pane.Section>
+            <Grid>
+              <Grid.Row>
+                <Grid.Col width={1} mb={20}>
+                  <FormRow
+                    caption={t('submitDapp.nameFieldCaption')}
+                    component={TextInput}
+                    label={t('submitDapp.nameFieldLabel')}
+                    name="name"
+                  />
+                </Grid.Col>
+              </Grid.Row>
+              <Grid.Row flexWrap="wrap">
+                <Grid.Col width={[1, 1, 1 / 2]} mb={20}>
+                  <FormRow
+                    caption={t('submitDapp.urlFieldCaption')}
+                    component={TextInput}
+                    label={t('submitDapp.urlFieldLabel')}
+                    name="url"
+                  />
+                </Grid.Col>
+                <Grid.Col width={[1, 1, 1 / 2]} mb={20}>
+                  <FormRow
+                    caption={t('submitDapp.websiteUrlFieldCaption')}
+                    component={TextInput}
+                    label={t('submitDapp.websiteUrlFieldLabel')}
+                    name="websiteUrl"
+                  />
+                </Grid.Col>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Col width={1} mb={20}>
+                  <FormRow
+                    caption={t('submitDapp.taglineFieldCaption')}
+                    component={TextInput}
+                    label={t('submitDapp.taglineFieldLabel')}
+                    name="tagline"
+                  />
+                </Grid.Col>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Col width={1}>
+                  <FormRow
+                    caption={t('submitDapp.descriptionFieldCaption')}
+                    component={Textarea}
+                    label={t('submitDapp.descriptionFieldLabel')}
+                    name="description"
+                  />
+                </Grid.Col>
+              </Grid.Row>
+            </Grid>
+          </Pane.Section>
+          <Pane.Section>
+            <Grid>
+              <Grid.Row>
+                <Grid.Col width={1}>
+                  <FormRow
+                    caption={t('submitDapp.imageUrlFieldCaption')}
+                    component={TextInput}
+                    label={t('submitDapp.imageUrlFieldLabel')}
+                    name="imageUrl"
+                  />
+                </Grid.Col>
+              </Grid.Row>
+            </Grid>
+          </Pane.Section>
+          <Pane.Section>
+            <Grid>
+              <Grid.Row>
+                <Grid.Col width={1}>
+                  <FormRow
+                    caption={t('submitDapp.supportUrlFieldCaption')}
+                    component={TextInput}
+                    label={t('submitDapp.supportUrlFieldLabel')}
+                    name="supportUrl"
+                  />
+                </Grid.Col>
+              </Grid.Row>
+            </Grid>
+          </Pane.Section>
+          <Pane.Section>
+            <Button onClick={handleSubmit} type="submit" wide>
+              {t('submitDapp.submitButton')}
+            </Button>
+          </Pane.Section>
+        </form>
+      </Pane>
     );
   }
 }
