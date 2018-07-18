f7c7ae596c5c0013660e97f4f5b07e4a8a600766
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-18 17:17:39 +0200

    [MOB-295] Refactor loadMasterAccount action into separate saga

--- packages/web-wallet/src/state/sagas/index.js
+++ packages/web-wallet/src/state/sagas/index.js
@@ -1,29 +1,31 @@
 import { all } from 'redux-saga/effects';
 
-import initAppSaga from './initApp';
-import watchAccountSaga from './watchAccount';
-import signupSaga from './signup';
-import downloadKeypairSaga from './downloadKeypair';
-import submitTransactionSaga from './submitTransaction';
-import loginSaga from './login';
-import requestSaga from './request';
-import loadAppsSaga from './loadApps';
 import depositAppSaga from './depositApp';
+import downloadKeypairSaga from './downloadKeypair';
+import initAppSaga from './initApp';
+import loadAppsSaga from './loadApps';
+import reloadMasterAccountSaga from './reloadMasterAccount';
+import loginSaga from './login';
 import openDappSaga from './openDapp';
 import releaseBalanceSaga from './releaseBalance';
+import requestSaga from './request';
+import signupSaga from './signup';
+import submitTransactionSaga from './submitTransaction';
+import watchAccountSaga from './watchAccount';
 
 export default function* () {
   yield all([
-    initAppSaga,
-    watchAccountSaga,
-    signupSaga,
-    downloadKeypairSaga,
-    submitTransactionSaga,
-    loginSaga,
-    requestSaga,
-    loadAppsSaga.listener,
     depositAppSaga,
+    downloadKeypairSaga,
+    initAppSaga,
+    loadAppsSaga.listener,
+    loginSaga,
     openDappSaga,
     releaseBalanceSaga.listener,
+    requestSaga,
+    signupSaga,
+    submitTransactionSaga,
+    reloadMasterAccountSaga,
+    watchAccountSaga,
   ]);
 }
