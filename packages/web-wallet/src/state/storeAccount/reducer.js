59b3783a80ea5170483d3a18ffe22301a8618d15
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-19 00:20:10 +0200

    [MOB-295] Add storeAccount reducer w/ updates subscription

---
+++ packages/web-wallet/src/state/storeAccount/reducer.js
@@ -0,0 +1,14 @@
+import { merge } from 'state/utils';
+import { createReducer } from 'redux-yo';
+
+import { storeAccountActions } from './actions';
+
+const initialState = {};
+
+export const storeAccountReducer = createReducer(
+  {
+    [storeAccountActions.setStoreAccount]: (state, storeAccount) =>
+      merge(state, storeAccount),
+  },
+  initialState
+);
