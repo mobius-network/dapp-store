1786cbdb1ffed80caedf03942b2607861a0687c1
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-18 23:45:27 +0200

    [MOB-295] Add Dapp submission 1 & 2 steps screens

---
+++ packages/web-wallet/src/components/Developers/SubmitDapp/Form/FormContainer.js
@@ -0,0 +1,39 @@
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { reduxForm, SubmissionError } from 'redux-form';
+import { translate } from 'react-i18next';
+import { promisifyAction } from 'redux-yo';
+import { createStructuredSelector } from 'reselect';
+
+import { getIsFetching } from 'state/requests';
+import { submitDappActions } from 'state/submitDapp';
+import { validate } from './validations';
+
+import Form from './Form';
+
+const mapStateToProps = createStructuredSelector({
+  isDataUploadingToIpfs: state =>
+    getIsFetching(state, { operation: 'addFilesToIpfs' }),
+  isSubmitting: state => getIsFetching(state, { operation: 'submitDapp' }),
+  isUserAccountFunding: state =>
+    getIsFetching(state, { operation: 'fundUserAccount' }),
+  isUserAccountLoading: state =>
+    getIsFetching(state, { operation: 'loadUserAccount' }),
+});
+
+const actions = {
+  ...submitDappActions,
+};
+
+export default compose(
+  connect(mapStateToProps, actions),
+  translate('translation'),
+  reduxForm({
+    form: 'submitDapp',
+    validate,
+    onSubmit: (values, store, { submitDapp }) =>
+      promisifyAction(submitDapp, values).catch(error => {
+        throw new SubmissionError(error);
+      }),
+  })
+)(Form);
