019e27a1eb8c0e5f9999d3cb4bc4e89d356ec2de
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-19 17:46:09 +0200

    [MOB-295] Fix & refactor ENV vars passing for different environments

--- config/beta.env.js
+++ config/beta.env.js
@@ -1,14 +1,9 @@
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const baseEnv = require('./base.env');
 
-const gitRevisionPlugin = new GitRevisionPlugin();
-
-module.exports = {
+module.exports = Object.assign(baseEnv, {
   API_URL: '"https://beta.mobius.network/api/v1"',
-  COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
-  HONEYBADGER_API_TOKEN: '"d52d00a4"',
   MOBI_ASSET_ISSUER: '"GDRWBLJURXUKM4RWDZDTPJNX6XBYFO3PSE4H4GPUL6H6RCUQVKTSD4AT"',
-  MOBI_ASSET: '"MOBI"',
   NODE_ENV: '"beta"',
   STELLAR_HORIZON_URL: '"https://horizon-testnet.stellar.org"',
   STELLAR_TESTNET: '"true"',
-};
+});
