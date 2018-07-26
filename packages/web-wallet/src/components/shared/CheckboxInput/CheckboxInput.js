d120bcc82fcf99e7f6fff57e4cadd08e1491942d
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-26 17:51:49 +0200

    [MOB-295] Add shared CheckboxInput component

---
+++ packages/web-wallet/src/components/shared/CheckboxInput/CheckboxInput.js
@@ -0,0 +1,57 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { faCheck } from '@fortawesome/fontawesome-free-solid';
+
+import {
+  Control,
+  ControlIcon,
+  ErrorMessage,
+  HiddenCheckbox,
+  Label,
+  Text,
+} from './styles';
+
+class CheckboxInput extends Component {
+  static propTypes = {
+    disabled: PropTypes.bool,
+    input: PropTypes.object,
+    inputProps: PropTypes.object,
+    meta: PropTypes.shape({
+      error: PropTypes.string,
+      dirty: PropTypes.bool,
+      touched: PropTypes.bool,
+    }),
+    placeholder: PropTypes.string,
+  };
+
+  render() {
+    const {
+      className,
+      input,
+      inputProps,
+      meta: { error, dirty, touched },
+      name,
+      ...rest
+    } = this.props;
+
+    const showError = error && (dirty || touched);
+
+    return (
+      <Fragment>
+        <Label htmlFor={name} className={className}>
+          <HiddenCheckbox type="checkbox" {...input} {...rest} />
+
+          <Control error={showError}>
+            {input.value && <ControlIcon icon={faCheck} />}
+          </Control>
+          <Text>{inputProps.label}</Text>
+        </Label>
+
+        {showError && <ErrorMessage>{error}</ErrorMessage>}
+      </Fragment>
+    );
+  }
+}
+
+export default CheckboxInput;
