97a368a455014e9f681e037322ca98054405039f
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-26 19:20:52 +0200

    [MOB-295] Add DappForm w/ AgreementModal to DetailsForm step

--- packages/web-wallet/src/components/Developers/SubmitDapp/DetailsForm/DetailsFormContainer.js
+++ packages/web-wallet/src/components/Developers/SubmitDapp/DetailsForm/DetailsFormContainer.js
@@ -1,42 +1,26 @@
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { reduxForm, SubmissionError } from 'redux-form';
 import { translate } from 'react-i18next';
-import { promisifyAction } from 'redux-yo';
+import { submit } from 'redux-form';
 import { createStructuredSelector } from 'reselect';
 
 import { getIsFetching } from 'state/requests';
-import { submitDappActions } from 'state/submitDapp';
-
-import { validate } from './validations';
+import { submitDappActions, getDAppIsSubmitting } from 'state/submitDapp';
 
 import DetailsForm from './DetailsForm';
 
 const mapStateToProps = createStructuredSelector({
-  isDataUploadingToIpfs: state =>
-    getIsFetching(state, { operation: 'addFilesToIpfs' }),
-  isSubmitting: state => getIsFetching(state, { operation: 'submitDapp' }),
-  isUserAccountFunding: state =>
-    getIsFetching(state, { operation: 'fundUserAccount' }),
-  isUserAccountLoading: state =>
-    getIsFetching(state, { operation: 'loadUserAccount' }),
+  isSubmitting: getDAppIsSubmitting,
   isUserAccountMerging: state =>
     getIsFetching(state, { operation: 'mergeUserAccount' }),
 });
 
 const actions = {
   ...submitDappActions,
+  submitDappForm: () => submit('dappForm'),
 };
 
 export default compose(
   connect(mapStateToProps, actions),
-  translate('translation'),
-  reduxForm({
-    form: 'submitDapp',
-    validate,
-    onSubmit: (values, store, { submitDapp }) =>
-      promisifyAction(submitDapp, values).catch(error => {
-        throw new SubmissionError(error);
-      }),
-  })
+  translate('translation')
 )(DetailsForm);
--- packages/web-wallet/src/state/submitDapp/selectors.js
+++ packages/web-wallet/src/state/submitDapp/selectors.js
@@ -3,6 +3,7 @@ import { isNil, isEmpty } from 'lodash';
 import { parseBalance, parsedBalanceValue } from '@mobius-network/core';
 
 import { getDappCatalog } from 'state/storeAccount';
+import { getIsFetching } from 'state/requests';
 
 export const getSubmitStep = state => state.submitDapp.submitStep;
 
@@ -56,4 +57,21 @@ export const getIsDappSubmitted = createSelector(
 
     return true;
   }
+);
+
+export const getDAppIsSubmitting = createSelector(
+  state => getIsFetching(state, { operation: 'addFilesToIpfs' }),
+  state => getIsFetching(state, { operation: 'submitDapp' }),
+  state => getIsFetching(state, { operation: 'fundUserAccount' }),
+  state => getIsFetching(state, { operation: 'loadUserAccount' }),
+  (
+    isDataUploadingToIpfs,
+    isSubmitting,
+    isUserAccountFunding,
+    isUserAccountLoading
+  ) =>
+    isDataUploadingToIpfs ||
+    isSubmitting ||
+    isUserAccountFunding ||
+    isUserAccountLoading
 );
