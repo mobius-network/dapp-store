886bfaade7b82823ba140426848b295d87df6c39
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-07-27 02:23:23 +0200

    [MOB-294] Add theming to shared Pic component

--- packages/web-wallet/src/components/shared/Pic/Pic.js
+++ packages/web-wallet/src/components/shared/Pic/Pic.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
 import { isString } from 'lodash';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/fontawesome-free-regular';
 
-import { Container, Image, Placeholder } from './styles';
+import { Container, Image, Placeholder, themes } from './styles';
 
 class Pic extends Component {
   static propTypes = {
     className: PropTypes.string,
+    theme: PropTypes.oneOf(['default', 'small']),
     url: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    theme: 'default',
   };
 
   constructor(props) {
@@ -23,19 +29,21 @@ class Pic extends Component {
   handleImageError = () => this.setState({ isPlaceholderVisible: true });
 
   render() {
-    const { className, url } = this.props;
+    const { className, url, theme } = this.props;
     const { isPlaceholderVisible } = this.state;
 
     return (
-      <Container className={className}>
-        {isPlaceholderVisible ? (
-          <Placeholder>
-            <FontAwesomeIcon icon={faImage} />
-          </Placeholder>
-        ) : (
-          <Image src={url} onError={this.handleImageError} />
-        )}
-      </Container>
+      <ThemeProvider theme={themes[theme]}>
+        <Container className={className}>
+          {isPlaceholderVisible ? (
+            <Placeholder>
+              <FontAwesomeIcon icon={faImage} />
+            </Placeholder>
+          ) : (
+            <Image src={url} onError={this.handleImageError} />
+          )}
+        </Container>
+      </ThemeProvider>
     );
   }
 }
