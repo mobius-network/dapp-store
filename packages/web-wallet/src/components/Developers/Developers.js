de668d481aed565ac4d4c79d2a2726c834ba4a63
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-08-02 05:20:46 +0200

    [MOB-294] Add Dapp Edit routes

--- packages/web-wallet/src/components/Developers/Developers.js
+++ packages/web-wallet/src/components/Developers/Developers.js
@@ -5,12 +5,18 @@ import PrivateRoute from 'components/shared/PrivateRoute';
 
 import MyDapps from './MyDapps';
 import SubmitDapp from './SubmitDapp';
+import EditDapp from './EditDapp';
 
 class Developers extends Component {
   render() {
     return (
       <Switch>
         <PrivateRoute path="/developers/my" component={MyDapps} exact />
+        <PrivateRoute
+          path="/developers/my/:id/edit"
+          component={EditDapp}
+          exact
+        />
         <PrivateRoute path="/developers/submit" component={SubmitDapp} exact />
       </Switch>
     );
