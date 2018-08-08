fb4bdd2b093ddc1240aacc78363bfb79a85e4a6f
Valery Bugakov <skymk1@gmail.com>
2018-08-08 14:33:07 +0300

    Mobile: completed basic setup

--- packages/web-wallet/src/utils/i18n.js
+++ packages/web-wallet/src/utils/i18n.js
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18nextReactNative from 'i18next-react-native-language-detector';
 import en from 'locales/en.json';
 import ch from 'locales/ch.json';
 import { isDev } from './env';
 
-i18n.use(LanguageDetector).init({
+i18n.use(i18nextReactNative).init({
   debug: isDev,
   fallbackLng: 'en',
   interpolation: {
