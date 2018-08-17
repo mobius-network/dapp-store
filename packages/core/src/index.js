b945a1ed796c162f6da50f0cc569e96426e9dfca
Valery Bugakov <skymk1@gmail.com>
2018-08-17 09:16:59 +0300

    Mobile: replaced imports from core package with local mobules

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,6 +1,1 @@
 export * from './services';
-export * from './state/account';
-export * from './state/auth';
-export * from './state/notifications';
-export * from './state/transfers';
-export * from './state/sagas';
