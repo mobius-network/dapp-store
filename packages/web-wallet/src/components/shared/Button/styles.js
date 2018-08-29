c728c6ae883282505d6fcd7bf04caadae873a677
Gleb Pospelov <codefo@codefo.com>
2018-08-29 03:53:20 +0300

    [MOB-416] Added custom Alert component

--- packages/web-wallet/src/components/shared/Button/styles.js
+++ packages/web-wallet/src/components/shared/Button/styles.js
@@ -80,7 +80,7 @@ export const Content = styled.div`
   flex-direction: row;
   flex: 1;
   font-family: ${fonts.nunitoSans};
-  font-size: ${fontSizes.button};
+  font-size: ${fontSizes.normal};
   font-weight: ${props => props.theme.fontWeight};
   justify-content: center;
   margin: 2px;
